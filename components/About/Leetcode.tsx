5f2426cb8be43d3dddfe8215f8eb21ccbb0dc718
GVAmaresh <reshama0302@gmail.com>
2024-10-20 17:50:26 +0530

    changed leetcode design

--- components/About/Leetcode.tsx
+++ components/About/Leetcode.tsx
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 import { ApexOptions } from "apexcharts";
 import { LEETCODE_DETAILS } from "@/api/Leetcode_API/fetchAPI";
+import { IoCheckmarkDoneCircle } from "react-icons/io5";
 import { useColor } from "@/app/ColorContext";
 
 const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
@@ -68,7 +69,6 @@ const Leetcode = () => {
       }
     ]
   });
-
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -108,13 +108,49 @@ const Leetcode = () => {
           : 0;
 
         setSeries([easyPercentage, mediumPercentage, hardPercentage]);
+
+        setOptions((prevOptions) => ({
+          ...prevOptions,
+          chart: {
+            ...(prevOptions?.chart ?? {})
+          },
+          plotOptions: {
+            radialBar: {
+              ...(prevOptions?.plotOptions?.radialBar ?? {}),
+              hollow: {
+                ...(prevOptions?.plotOptions?.radialBar?.hollow ?? {}),
+                size: "35%",
+                margin: 5,
+                background: "transparent"
+              },
+              dataLabels: {
+                ...(prevOptions?.plotOptions?.radialBar?.dataLabels ?? {}),
+                name: { show: false },
+                value: { show: false },
+                total: {
+                  show: true,
+                  label: "",
+                  formatter: function () {
+                    return `fffffff`;
+                  },
+                  style: {
+                    fontSize: "22px",
+                    fontWeight: "bold",
+                    color: theme.hardColor
+                  }
+                }
+              }
+            }
+          },
+          colors: [theme.easyColor, theme.mediumColor, theme.hardColor]
+        }));
       } catch (error) {
         console.error("Error fetching LeetCode details:", error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [theme]);
 
   return (
     <div className="">
@@ -125,37 +161,54 @@ const Leetcode = () => {
         Leetcode
       </div>
       <div className="" style={{ boxShadow: theme.boxShadow }}>
-        <div className="">
-        </div>
+        <div className=""></div>
         <div className="flex flex-col md:flex-row justify-center">
-          <div id="chart">
+          <div id="chart" className="relative">
             <ApexCharts
               options={options}
               series={series}
               type="radialBar"
               height={390}
             />
+            <div className="absolute inset-0 flex items-center justify-center">
+              <div className="">
+                <div className="">
+                  <span
+                    className="text-3xl font-extrabold"
+                    style={{ color: theme.heading }}
+                  >
+                    {leetcodeData ? leetcodeData.solvedProblems : "00"}
+                  </span>
+                  <span className="text-xs font-normal mt-3 text-white">
+                    /{leetcodeData ? leetcodeData.totalProblems : "000"}
+                  </span>
+                </div>
+                <div className=" text-green-500 flex pl-2">
+                  <span>Solved</span>
+                  <span className="mt-1">
+                    <IoCheckmarkDoneCircle color="green" />
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="mt-8 gap-3">
             <div className="mt-2 p-3 bg-slate-500 text-white rounded-lg">
               <span className="text-cyan-400 font-extrabold">Easy:{"  "}</span>
-              {leetcodeData ? leetcodeData.solvedEasy : "00"}/
-              {leetcodeData ? leetcodeData.totalEasy : "000"}
+              <span className=" text-2xl font-bold">{leetcodeData ? leetcodeData.solvedEasy : "00"}</span> 
+              <span className=" text-sm font-normal"> /{leetcodeData ? leetcodeData.totalEasy : "000"}</span> 
             </div>
             <div className="mt-2 p-3 bg-slate-500 text-white rounded-lg">
-              <span className="text-amber-500 font-extrabold">Medium:{"  "}</span>
-              {leetcodeData ? leetcodeData.solvedMedium : "000"}/
-              {leetcodeData ? leetcodeData.totalMedium : "000"}
+              <span className="text-amber-500 font-extrabold">
+                Medium:{"  "}
+              </span>
+              <span className=" text-2xl font-bold">{leetcodeData ? leetcodeData.solvedMedium: "00"}</span> 
+              <span className=" text-sm font-normal"> /{leetcodeData ? leetcodeData.totalMedium : "000"}</span> 
             </div>
             <div className="mt-2 p-3 bg-slate-500 text-white rounded-lg">
               <span className="text-red-700 font-extrabold">Hard:{"  "}</span>
-              {leetcodeData ? leetcodeData.solvedHard : "00"}/
-              {leetcodeData ? leetcodeData.totalHard : "000"}
-            </div>
-            <div className="mt-2 p-3 bg-slate-500 text-white rounded-lg">
-              <span className="text-green-500 font-extrabold">Total:{"  "}</span>
-              {leetcodeData ? leetcodeData.solvedProblems : "00"}/
-              {leetcodeData ? leetcodeData.totalProblems : "000"}
+              <span className=" text-2xl font-bold">{leetcodeData ? leetcodeData.solvedHard : "00"}</span> 
+              <span className=" text-sm font-normal"> /{leetcodeData ? leetcodeData.totalHard : "000"}</span> 
             </div>
           </div>
         </div>
